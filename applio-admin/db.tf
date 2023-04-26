984341c2efac8e29de737c6fa8d51dcf2f6f5943
Dennis von Bargen <dennis.von-bargen@viati.se>
2023-04-26 10:01:51 +0200

    Fixed typos

--- applio-admin/db.tf
+++ applio-admin/db.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_database_db" "db" {
-  cluster_id = var.postgres_db_cluster_id
+  cluster_id = var.postgres_cluster_id
   name       = "applio-admin-${var.env}"
 }
 
 resource "digitalocean_database_user" "db_user" {
-  cluster_id = var.postgres_db_cluster_id
+  cluster_id = var.postgres_cluster_id
   name       = "applio-admin-${var.env}"
 }
--- applio-data/db.tf
+++ applio-data/db.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_database_db" "db" {
-  cluster_id = var.postgres_db_cluster_id
+  cluster_id = var.postgres_cluster_id
   name       = "applio-data-${var.env}"
 }
 
 resource "digitalocean_database_user" "db_user" {
-  cluster_id = var.postgres_db_cluster_id
+  cluster_id = var.postgres_cluster_id
   name       = "applio-data-${var.env}"
 }
