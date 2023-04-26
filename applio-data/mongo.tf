54dacf151abda163e6d05c40a7d34d8ecfda7d3d
Dennis von Bargen <dennis.von-bargen@viati.se>
2023-04-26 10:23:40 +0200

    Fixed broken reference

--- applio-data/mongo.tf
+++ applio-data/mongo.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_database_db" "mongo" {
-  cluster_id = var.redis_cluster_id
+  cluster_id = var.mongo_cluster_id
   name       = "applio-data-${var.env}"
 }
 
 resource "digitalocean_database_user" "mongo_user" {
-  cluster_id = var.redis_cluster_id
+  cluster_id = var.mongo_cluster_id
   name       = "applio-data-${var.env}"
 }
